82dd50b03e7a90bb2183d5216a09ee1462bbc89c
BSuryakumari <136813935+BSuryakumari@users.noreply.github.com>
2024-04-30 19:35:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import TaskForm from './TaskForm';
-import TaskList from './TaskList';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 import './App.css';
 
 const App = () => {
