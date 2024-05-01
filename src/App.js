190010122872fdf09a199d5d1a801d893e127e82
BSuryakumari <136813935+BSuryakumari@users.noreply.github.com>
2024-05-01 16:02:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './App.css'
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
-import './App.css';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
 
-  const addTask = (taskText, assignedTo) => {
-    const newTask = {
-      id: tasks.length + 1,
-      text: taskText,
-      assignedTo: assignedTo,
-      status: 'pending', 
-    };
+  const addTask = (newTask) => {
     setTasks([...tasks, newTask]);
   };
 
-  const updateTaskStatus = (taskId, status) => {
-    const updatedTasks = tasks.map((task) => {
+  const completeTask = (taskId) => {
+    const updatedTasks = tasks.map(task => {
       if (task.id === taskId) {
-        return { ...task, status: status };
+        return { ...task, completed: true };
       }
       return task;
     });
@@ -28,9 +22,11 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <h1>Task List</h1>
-      <TaskForm addTask={addTask} />
-      <TaskList tasks={tasks} updateTaskStatus={updateTaskStatus} />
+      <div className='form-section'>
+        <h1 className="main-heading">Task Manager</h1>
+        <TaskForm addTask={addTask} />
+      </div>
+      <TaskList tasks={tasks} completeTask={completeTask} />
     </div>
   );
 };
